03a0ef634b0eea7b02526d57e904d58f545d646b
TheNinza <niks.a3198@gmail.com>
2021-11-13 14:44:25 +0530

    removed unused code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { auth, firestore } = require("./configs/firebase.js");
 const { decodeJWT } = require("./middlewares/decodeJWT.js");
 const suggestionsRoutes = require("./routes/suggestions.js");
 const { FRONT_END } = require("./configs/environments.js");
