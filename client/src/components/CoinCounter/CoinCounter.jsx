d76ab9059758c2c1b6e7a166da6484444b4b60ec
TheNinza <niks.a3198@gmail.com>
2021-12-10 19:32:51 +0530

    linked credit counter to database

--- client/src/components/CoinCounter/CoinCounter.jsx
+++ client/src/components/CoinCounter/CoinCounter.jsx
@@ -2,6 +2,8 @@ import { CircularProgress, makeStyles, Dialog } from "@material-ui/core";
 import { lazy, useEffect, useState } from "react";
 import { ReactComponent as Coin } from "../../assets/vsCoin.svg";
 import { Suspense } from "react";
+import { useSelector } from "react-redux";
+import { firestore } from "../../lib/firebase/firebase";
 // lazy imports
 const PaymentIntegrationModal = lazy(() =>
   import("../PaymentIntegrationModal/PaymentIntegrationModal")
@@ -46,7 +48,6 @@ const useStyles = makeStyles((theme) => ({
   coin: {
     width: "1.5rem",
     height: "1.5rem",
-    marginRight: "0.5rem",
     transition: "all 0.5s ease",
   },
   "@keyframes fadeIn": {
@@ -67,6 +68,7 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
 
     transition: "all 0.5s ease",
+    gap: "0.5rem",
   },
 }));
 
@@ -80,6 +82,9 @@ const CoinCounter = ({ position = "absolute" }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const [isOpen, setIsOpen] = useState(false);
+
+  const currentUserId = useSelector((state) => state.user.userData.uid);
+  const currentUserRole = useSelector((state) => state.user.userData.role);
 
   // animate coin counter
   useEffect(() => {
@@ -107,28 +112,53 @@ const CoinCounter = ({ position = "absolute" }) => {
     };
   }, [coins, finalCoins]);
 
+  useEffect(() => {
+    // add firestore listner to listen for credits
+    let unsubscribe = () => {};
+    if (currentUserRole === "user") {
+      unsubscribe = firestore
+        .collection("accounts")
+        .doc(currentUserId)
+        .onSnapshot(
+          (snapshot) => {
+            const data = snapshot.data();
+            if (data) {
+              setFinalCoins(data.credits);
+              setIsLoading(false);
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    }
+
+    if (currentUserRole === "unlimited") {
+      setIsLoading(false);
+    }
+
+    return () => {
+      unsubscribe();
+    };
+  }, [currentUserId, currentUserRole]);
+
   return (
     <>
       <div className={classes.root} style={{ position }}>
         {!isLoading ? (
           <div
-            onClick={() =>
-              // set coins to random integer from 1 to 50
-              setIsOpen(true)
-            }
+            onClick={() => currentUserRole === "user" && setIsOpen(true)}
             className={classes.container}
           >
-            <Coin className={classes.coin} /> <span> x {coins}</span>
+            <Coin className={classes.coin} />x
+            <span
+              style={{ fontSize: currentUserRole === "unlimited" ? 20 : 16 }}
+            >
+              {currentUserRole === "user" ? coins : "∞"}
+            </span>
           </div>
         ) : (
-          <CircularProgress
-            size={20}
-            onClick={() => {
-              // set coins to random integer from 1 to 50
-              setFinalCoins(Math.floor(Math.random() * 100) + 1);
-              setIsLoading(false);
-            }}
-          />
+          <CircularProgress size={20} />
         )}
       </div>
 
